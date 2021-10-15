7fdb0e74a90902e33d6a111bb2bfe8b9f8f55962
syson <setong17@gmail.com>
2021-10-15 15:44:53 +0900

    TODO 리스트 저장

--- src/main/java/com/springboot/todolist/persistence/TodoRepository.java
+++ src/main/java/com/springboot/todolist/persistence/TodoRepository.java
@@ -2,7 +2,6 @@ package com.springboot.todolist.persistence;
 
 import com.springboot.todolist.model.TodoEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,7 +9,7 @@ import java.util.List;
 @Repository
 public interface TodoRepository extends JpaRepository<TodoEntity, String> {
 
-    @Query("select * from Todo t where t.userId = ?1")
+    //@Query("select t from Todo t where t.userId = ?1")
     List<TodoEntity> findByUserId(String userId);
 
 }
